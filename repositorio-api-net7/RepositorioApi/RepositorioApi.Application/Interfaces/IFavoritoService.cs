145244917be3fc54a4c7fda1a9c8931aa15bdced
Matheus Angeli <matheusfariaangelisilva@gmail.com>
2025-07-21 02:03:24 -0300

    remoção de linhas de código comentadas não utilizadas

--- repositorio-api-net7/RepositorioApi/RepositorioApi.Application/Interfaces/IFavoritoService.cs
+++ repositorio-api-net7/RepositorioApi/RepositorioApi.Application/Interfaces/IFavoritoService.cs
@@ -1,12 +1,4 @@
-﻿using RepositorioApi.Application.DTOs;
-using RepositorioApi.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RepositorioApi.Application.Interfaces
+﻿namespace RepositorioApi.Application.Interfaces
 {
     public interface IFavoritoService
     {
--- repositorio-api-net7/RepositorioApi/RepositorioApi.Application/Interfaces/IRepositorioService.cs
+++ repositorio-api-net7/RepositorioApi/RepositorioApi.Application/Interfaces/IRepositorioService.cs
@@ -1,9 +1,4 @@
 ﻿using RepositorioApi.Application.DTOs;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RepositorioApi.Application.Interfaces
 {
